74644510285dc466e93c218cfd7820d6751be73c
Bill Zissimopoulos <billziss@navimatics.com>
2022-02-09 10:08:18 +0000

    assembler: labels

--- 6502as.py
+++ 6502as.py
@@ -11,6 +11,22 @@ insmap = __import__("6502astab").insmap
 class SyntaxError(Exception):
     pass
 
+line = 0
+minaddr = 0x10000
+maxaddr = -1
+addr = 0
+text = bytearray(64 * 1024)
+labels = {}
+fixups = {}
+fixup = ""
+
+def addlabel(arg, a):
+    labels[arg] = a
+
+def addfixup(arg, a, n):
+    l = fixups.setdefault(arg, [])
+    l.append((a, n))
+
 def value(arg):
     try:
         if arg.startswith("$"):
@@ -20,6 +36,13 @@ def value(arg):
     except:
         raise SyntaxError("invalid value: %r" % arg)
 
+def avalue(arg):
+    if arg.isidentifier():
+        global fixup
+        fixup = arg
+        return 0x10000
+    return value(arg)
+
 def mode(arg):
     if not arg:
         return ".", 0
@@ -28,42 +51,36 @@ def mode(arg):
     elif arg.startswith("("):
         if arg.endswith(",X)"):
             a = arg[1:-3]
-            v = value(a)
+            v = avalue(a)
             zpg = 0xff >= v and (3 >= len(a) or "$" != a[0])
             return ("(zp,x)" if zpg else "(a,x)"), v
         elif arg.endswith(")"):
             a = arg[1:-1]
-            v = value(a)
+            v = avalue(a)
             zpg = 0xff >= v and (3 >= len(a) or "$" != a[0])
             return ("(zp)" if zpg else "(a)"), v
         elif arg.endswith("),Y"):
             a = arg[1:-3]
-            v = value(a)
+            v = avalue(a)
             return "(zp),y", v
         else:
             raise SyntaxError("invalid operand: %r", arg)
     else:
         if arg.endswith(",X"):
             a = arg[:-2]
-            v = value(a)
+            v = avalue(a)
             zpg = 0xff >= v and (3 >= len(a) or "$" != a[0])
             return ("zp,x" if zpg else "a,x"), v
         if arg.endswith(",Y"):
             a = arg[:-2]
-            v = value(a)
+            v = avalue(a)
             zpg = 0xff >= v and (3 >= len(a) or "$" != a[0])
             return ("zp,y" if zpg else "a,y"), v
         else:
             a = arg
-            v = value(a)
+            v = avalue(a)
             zpg = 0xff >= v and (3 >= len(a) or "$" != a[0])
             return ("zp" if zpg else "a"), v
-
-line = 0
-minaddr = 0x10000
-maxaddr = -1
-addr = 0
-text = bytearray(64 * 1024)
 
 def directive(dir, arg):
     global minaddr, maxaddr, addr, text
@@ -102,17 +119,23 @@ def assemble(f):
         if opc.startswith("."):
             directive(opc, arg)
             continue
+        elif opc.endswith(":"):
+            addlabel(opc[:-1], addr)
+            continue
         mod, val = mode(arg)
         key = "%s %s" % (opc, mod)
         ins = insmap.get(key)
-        if not ins and "zp" == mod:
-            # zp and r address modes look similar; try both
+        if not ins:
+            # also try r mode
             mod = "r"
             key = "%s %s" % (opc, mod)
             ins = insmap.get(key)
         if ins:
             minaddr = min(minaddr, addr)
             text[addr] = ins[0]
+            if 0x10000 == val:
+                global fixup
+                addfixup(fixup, addr, ins[1] if "r" != mod else -ins[1])
             if 2 == ins[1]:
                 text[addr + 1] = val & 0xff
             elif 3 == ins[1]:
@@ -122,6 +145,18 @@ def assemble(f):
             maxaddr = max(maxaddr, addr - 1)
         else:
             raise SyntaxError("invalid instruction: %r" % l)
+    for lbl in sorted(fixups):
+        val = labels.get(lbl)
+        if val is None:
+            raise SyntaxError("invalid label: %r" % lbl)
+        for faddr, n in fixups[lbl]:
+            if 2 == n:
+                text[faddr + 1] = val & 0xff
+            elif -2 == n:
+                text[faddr + 1] = (val - faddr - 2) & 0xff
+            elif 3 == n:
+                text[faddr + 1] = val & 0xff
+                text[faddr + 2] = (val >> 8) & 0xff
 
 def emit():
     global minaddr, maxaddr, text
